159050ca521781fe2d2b675165d1a42f2833720b
DMawi17 <asegda17@gmail.com>
2021-09-11 20:08:53 +0200

    ejs set up

--- app.js
+++ app.js
@@ -2,39 +2,24 @@ const express = require("express");
 
 const app = express();
 
+// register view engine
+app.set("view engine", "ejs"); // create templates
+
 app.listen(3000);
 
 app.get("/", (req, res) => {
-    /* res.write("")
-    res.end()
-    //.. replace the above 2 methods
-    res.send will set header the content type auto.
-    also the status code, which is 200 */
-    // res.send(`<p>Home Page</p>`);
-    res.sendFile("./views/index.html", { root: __dirname });
+    res.render("index");
 });
-
-/*
-   ____             _   _             
-  |  _ \ ___  _   _| |_(_)_ __   __ _ 
-  | |_) / _ \| | | | __| | '_ \ / _` |
-  |  _ < (_) | |_| | |_| | | | | (_| |
-  |_| \_\___/ \__,_|\__|_|_| |_|\__, |
-                                |___/ 
-AND HTML
-*/
 
 app.get("/about", (req, res) => {
-    // res.send("<p>About Page</p>");
-    res.sendFile("./views/about.html", { root: __dirname });
+    res.render("about");
 });
 
-// REDIRECT from /about-us to /about
-app.get("/about-us", (req, res) => {
-    res.redirect("/about");
-});
+app.get('/blogs/create', (req, res) => {
+    res.render('create')
+})
 
 // 404 PAGE
 app.use((req, res) => {
-    res.status(404).sendFile("./views/404.html", { root: __dirname });
+    res.status(404).res.render("index");
 });
