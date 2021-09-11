46f772a77ed820ac9b50dbabb9c246060859b57d
DMawi17 <asegda17@gmail.com>
2021-09-11 20:23:30 +0200

    ejs almost skiped

--- app.js
+++ app.js
@@ -1,25 +1,42 @@
 const express = require("express");
 
+// express app
 const app = express();
 
-// register view engine
-app.set("view engine", "ejs"); // create templates
-
+// listen for requests
 app.listen(3000);
 
+// register view engine
+app.set("view engine", "ejs");
+// app.set('views', 'myviews');
+
 app.get("/", (req, res) => {
-    res.render("index");
+    const blogs = [
+        {
+            title: "Yoshi finds eggs",
+            snippet: "Lorem ipsum dolor sit amet consectetur",
+        },
+        {
+            title: "Mario finds stars",
+            snippet: "Lorem ipsum dolor sit amet consectetur",
+        },
+        {
+            title: "How to defeat bowser",
+            snippet: "Lorem ipsum dolor sit amet consectetur",
+        },
+    ];
+    res.render("index", { title: "Home", blogs });
 });
 
 app.get("/about", (req, res) => {
-    res.render("about");
+    res.render("about", { title: "About" });
 });
 
-app.get('/blogs/create', (req, res) => {
-    res.render('create')
-})
+app.get("/blogs/create", (req, res) => {
+    res.render("create", { title: "Create a new blog" });
+});
 
-// 404 PAGE
+// 404 page
 app.use((req, res) => {
-    res.status(404).res.render("index");
+    res.status(404).render("404", { title: "404" });
 });
