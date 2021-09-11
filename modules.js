c32755d44e85f541315e28f3624363ce573ba56a
DMawi17 <asegda17@gmail.com>
2021-09-11 09:58:30 +0200

    OS sorted

--- modules.js
+++ modules.js
@@ -1,13 +1,15 @@
-const { people, ages } = require("./people");
-
-console.log(people, ages); // => without module.exports xyz is = an empty object {}
-
-// without module.exports, people doesn't work!!!
-// console.log(people);
-
-//... with module.exports xyz is = 'hello'
-// console.log(xyz); // => empty object {}
+/*
+ 
+    ___  ____  
+   / _ \/ ___| 
+  | | | \___ \ 
+  | |_| |___) |
+   \___/|____/ 
+               
+ 
+*/
 
 const os = require("os");
+console.log(os.platform(), os.homedir()); // darwin /Users/DMawi
 
-console.log(os);
+// explore TODO:
