0d26ab8ed9fa976c140ba174d3f8cb0fdbd6ff54
DMawi17 <asegda17@gmail.com>
2021-09-11 12:46:52 +0200

    routing sorted

--- server.js
+++ server.js
@@ -2,51 +2,36 @@ const http = require("http");
 const fs = require("fs");
 
 /*
-RESPONDING WITH
-   ____ _____ ____  
-  / ___|_   _|  _ \ 
-  \___ \ | | | |_) |
-   ___) || | |  _ < 
-  |____/ |_| |_| \_\                    
-*/
-
-/* const server = http.createServer((req, res) => {
-    // console.log(req); // vomits the entire objects
-    console.log(req.url); // `/` if on home page or `/about` if on about page
-    console.log(req.method); // GET
-
-    // set header content type
-    res.setHeader("Content-Type", "text/html");
-
-    res.write(`<head><link res="stylesheet" href="#"></head>`); // its in head
-    res.write("<p>Hello, ninja</p>");
-    res.write("<p>Hello, 2nd ninja</p>");
-    res.end(); // we can also write inside an end
-});
-
-server.listen(3000, "localhost", () => {
-    console.log("listening for request on port 3000");
-}); */
-
-/*
-RESPONDING WITH
-   _   _ _____ __  __ _     
-  | | | |_   _|  \/  | |    
-  | |_| | | | | |\/| | |    
-  |  _  | | | | |  | | |___ 
-  |_| |_| |_| |_|  |_|_____|
-FILE                           
+A SIMPLE
+   ____             _   _             
+  |  _ \ ___  _   _| |_(_)_ __   __ _ 
+  | |_) / _ \| | | | __| | '_ \ / _` |
+  |  _ < (_) | |_| | |_| | | | | (_| |
+  |_| \_\___/ \__,_|\__|_|_| |_|\__, |
+                                |___/ 
+SYSTEM
 */
 
 const server = http.createServer((req, res) => {
-    console.log(req.url); // `/` if on home page or `/about` if on about page
-    console.log(req.method); // GET
-
     // set header content type
     res.setHeader("Content-Type", "text/html");
 
+    let path = "./views/";
+
+    switch (req.url) {
+        case "/":
+            path += "index.html";
+            break;
+        case "/about":
+            path += "about.html";
+            break;
+        default:
+            path += "404.html";
+            break;
+    }
+
     // send an html file
-    fs.readFile("./views/index.html", (err, data) => {
+    fs.readFile(path, (err, data) => {
         if (err) {
             console.log(err);
             res.end();
