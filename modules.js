bf1e8d0f233c4538a8917c98e40ece0a7b431e41
DMawi17 <asegda17@gmail.com>
2021-09-11 10:35:22 +0200

    all braches are now sorted

--- modules.js
+++ modules.js
@@ -1,12 +1,10 @@
-// 1. node modules.js will execute people.js file because of the require.
-const xyz = require("./people");
+/* 
 
-// 2. return an empty object
-console.log(xyz);
 
-// 3. now xyz is people array
-console.log(xyz); // => [ 'yoshi', 'ryu', 'chun-li', 'mario' ]
+* Global
+* Module
+* OS
+* FS
 
-// 4. destructuring-import is now allowed
-const { people, ages } = require("./people");
-console.log(people, ages);
+
+*/
