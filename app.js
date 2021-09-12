8b3bdf171740e559f770a556efd3083ac7670afb
DMawi17 <asegda17@gmail.com>
2021-09-12 11:43:07 +0200

    POST method sorted

--- app.js
+++ app.js
@@ -4,17 +4,6 @@ const mongoose = require("mongoose");
 const Blog = require("./models/blog");
 
 const app = express();
-app.set("view engine", "ejs");
-app.use(morgan("dev"));
-
-/*
-   __  __  ___  _   _  ____  ___  
-  |  \/  |/ _ \| \ | |/ ___|/ _ \ 
-  | |\/| | | | |  \| | |  _| | | |
-  | |  | | |_| | |\  | |_| | |_| |
-  |_|  |_|\___/|_| \_|\____|\___/ 
-DB AND MONGOOSE
-*/
 
 // CONNECT TO MONGODB
 const dbURI =
@@ -24,74 +13,43 @@ const dbURI =
 mongoose
     .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
-        // console.log("connected to db");
         app.listen(3000, (req, res) =>
             console.log("connected to db and now listening")
         );
     })
     .catch((err) => console.log(err));
 
-/* // MONGOOSE AND MONGO SANDBOX ROUTES
-app.get("/add-blog", (req, res) => {
-    // new instance of a blog we imported from blog.js
-    const blog = new Blog({
-        title: "new blog 2",
-        snippet: "about my new blog",
-        body: "more about my new blog",
-    });
+// REGISTER VIEW ENGINE
+app.set("view engine", "ejs");
 
-    // SAVE blog TO A DB
-    blog.save()
-        .then((result) => {
-            res.send(result);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-});
-
-// FINDS ALL BLOGS
-app.get("/all-blogs", (req, res) => {
-    Blog.find()
-        .then((result) => res.send(result))
-        .catch((err) => console.log(err));
-});
-
-//FINDS A SINGLE BLOG
-app.get("/single-blog", (req, res) => {
-    Blog.findById("613d964887f8f3d0685dbfb4")
-        .then((result) => res.send(result))
-        .catch((err) => console.log(err));
-}); */
-
-// ...
-
+// MIDDLEWARE & STATIC FILES
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true })); // allow us to use `req.body`
+app.use(morgan("dev"));
 
-/* app.get("/", (req, res) => {
-    const blogs = [
-        {
-            title: "Yoshi finds eggs",
-            snippet: "Lorem ipsum dolor sit amet consectetur",
-        },
-        {
-            title: "Mario finds stars",
-            snippet: "Lorem ipsum dolor sit amet consectetur",
-        },
-        {
-            title: "How to defeat bowser",
-            snippet: "Lorem ipsum dolor sit amet consectetur",
-        },
-    ];
-    res.render("index", { title: "Home", blogs });
-}); */
+// ROUTES
+app.get("/", (req, res) => {
+    res.redirect("/blogs");
+});
 
 app.get("/about", (req, res) => {
     res.render("about", { title: "About" });
 });
 
 // BLOG ROUTES
-app.get("/", (req, res) => {
+app.get("/blogs/create", (req, res) => {
+    res.render("create", { title: "Create a new blog" });
+});
+
+app.post("/blogs", (req, res) => {
+    const blog = new Blog(req.body);
+
+    blog.save()
+        .then((result) => res.redirect("/blogs"))
+        .catch((err) => console.log(err));
+});
+
+app.get("/blogs", (req, res) => {
     Blog.find()
         .sort({ createdAt: -1 }) // reverse order with -1
         .then((result) =>
@@ -101,10 +59,6 @@ app.get("/", (req, res) => {
             })
         )
         .catch((err) => console.log(err));
-});
-
-app.get("/blogs/create", (req, res) => {
-    res.render("create", { title: "Create a new blog" });
 });
 
 app.use((req, res) => {
